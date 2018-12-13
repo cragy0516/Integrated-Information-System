b5751f73ff113b245030f360f6ce82c005b919cf
MitNy <33439062+MitNy@users.noreply.github.com>
2018-12-13 15:30:22 +0900

    Update apply

--- term09/src/main/java/Service/subjectService.java
+++ term09/src/main/java/Service/subjectService.java
@@ -1,5 +1,10 @@
 package Service;
 import Dao.Database;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
@@ -28,5 +33,34 @@ public class subjectService {
 			conn.close();
 		}
 		return false;
+	}
+	
+	public List<String> getTime(String userID) throws Exception {
+		List<String> timeList = new ArrayList<String>();
+		Database dbCon = new Database();
+		Connection conn = dbCon.GetConnection();
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			String getQuery="select lectureTime from subject where lectureNumber = any(select lectureNumber from course where studentID=?)";
+			ps = conn.prepareStatement(getQuery);
+			ps.setString(1, userID);
+			rs = ps.executeQuery();
+			while( rs.next() ) {
+				timeList.add(rs.getString("lectureTime"));
+			}
+			Pattern p = Pattern.compile("([ㄱ-ㅎ|ㅏ-ㅣ|가-힣]).([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]*[\\~].([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]");
+			
+			return timeList;
+		}
+		catch(Exception e ) {
+			System.out.print(e.getMessage());
+		}
+		finally {
+			ps.close();
+			rs.close();
+			conn.close();
+		}
+		return null;
 	}
 }
